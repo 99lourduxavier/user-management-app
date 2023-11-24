1880d3111efe81b8bed8ab549015af24fac8874c
99lourduxavier <45066102+99lourduxavier@users.noreply.github.com>
2023-11-24 06:22:20 -0800

    Add files via upload

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Component</h2>
+      {/* Add your login form */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h2>Signup Component</h2>
+      {/* Add your signup form */}
+    </div>
+  );
+};
+
+export default Signup;
+v
