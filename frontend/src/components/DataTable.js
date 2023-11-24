2c8d884af52aac477834acde9cc60e991dba82ed
99lourduxavier <45066102+99lourduxavier@users.noreply.github.com>
2023-11-24 06:21:18 -0800

    Create DataTable.js

---
+++ frontend/src/components/DataTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataTable = () => {
+  return (
+    <div>
+      <h2>Data Table Component</h2>
+      {/* Add your data table */}
+    </div>
+  );
+};
+
+export default DataTable;
